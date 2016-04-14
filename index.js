ca2858e8038ea56d1b4584f45ce7763328f03385
Pedro Henrique <pedroknk@gmail.com>
2016-04-14 18:26:57 -0300

    remove

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var app = express();
 
-var removeUseStrict = require('remove-use-strict');
-
-module.exports = function(content) {
-    return removeUseStrict(content, {force: true});
-};
-
 app.get('/', function (req, res) {
   res.send('Hello Worldd 80!!!');
 });
