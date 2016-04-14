17c2652666317028822ddc1ff72594cd6c6d1c4e
Pedro Henrique <pedroknk@gmail.com>
2016-04-14 18:25:01 -0300

    remove

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+var removeUseStrict = require('remove-use-strict');
+
+module.exports = function(content) {
+    return removeUseStrict(content, {force: true});
+};
+
 app.get('/', function (req, res) {
   res.send('Hello Worldd 80!!!');
 });
@@ -8,3 +14,4 @@ app.get('/', function (req, res) {
 app.listen(80, function () {
   console.log('Example app listening ---  on port 80!');
 });
+
